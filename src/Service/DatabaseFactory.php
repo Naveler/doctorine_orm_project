9c1927f0560f5311985633a030786cf9fd960aa6
Georg <georg.vilja@khk.ee>
2021-02-04 15:08:03 +0200

    Creted Entity Manager and added to index page container

---
+++ src/Service/DatabaseFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+class DatabaseFactory {
+	/**
+	* Create Doctrine Entity Entity Manager
+	* @return EntityManager
+	*
+	*/
+	public static function create(){
+		$isDevMode = true;
+		$metadata = Setup::createAnnotationMetadataConfiguration([__DIR__ . '/../Entity'], $isDevMode);
+
+		$dbParams = [
+			'driver' => 'pdo_mysql',
+			'host' => 'localhost',
+			'user' =>'georg',
+			'password' => 'qwerty',
+			'dbname' => 'doctrineorm',
+			'charset' => 'utf8'
+		];
+
+		return EntityManager::create($dbParams, $metadata);
+	}
+}
