9446dbefc2a3719630c2f8cbf816a671d655e471
Georg <georg.vilja@khk.ee>
2021-02-10 15:07:35 +0200

    loetud ja kuvatud yhe artikli andmed Query Builderi abil

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,15 +11,36 @@ class ArticleController extends Controller
     public function view(Request $request, Response $response
     	, $args = [])
     {
+    	$qb = $this->ci->get('db')->createQueryBuilder();
+
+    	$qb->select('a')
+    		->from('App\Entity\Article', 'a')
+    		->where('a.slug= :slug')
+    		->setParameter('slug', $args['slug']);
+
+    	$query = $qb->getQuery();
+
+    	$article = $query->getSingleResult();
+
+   		return $this->renderPage($response, 'article.html', [
+        	'article' => $article
+        ]);
+    }
+
+    public function viewRP(Request $request, Response $response
+    	, $args = [])
+    {
     	$article = $this->ci->get('db')->getRepository('App\Entity\Article')->findOneBy([
     			'slug' => $args['slug']
     	]);
-        return $this->renderPage($response, 'article.html', [
+
+    	if(!$article){
+    		throw new HttpNotFoundException($request);
+   		}
+
+   		return $this->renderPage($response, 'article.html', [
         	'article' => $article
         ]);
-
-    if(!$article){
-    	throw new HttpNotFoundException($request);
     }
 
     public function viewPK(Request $request, Response $response)
@@ -29,5 +50,6 @@ class ArticleController extends Controller
         	'article' => $article
         ]);
     }
+
 
 }
